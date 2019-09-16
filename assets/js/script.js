b1079015d002aab019fd1a887eb182cf54b143bd
gabilg <gabi_lima_xd@hotmail.com>
2019-09-16 00:18:21 -0300

    Insert Slick

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,21 @@ $(document).ready(function() {
         $('.logo-img').toggleClass('scroll', is_scroll);
     });
 
-    new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 4,
-    // autoplay: 3000,
-    gap: 30
-    }).mount()
+    // new Glide('.glide-sponsors', {
+    //     type: 'carousel',
+    //     startAt: 0,
+    //     perView: 4,
+    //     autoplay: 3000,
+    //     gap: 30
+    // }).mount()
+
+    // new Glide('.glide-banner', {
+    //     type: 'carousel',
+    //     startAt: 0,
+    //     perView: 1,
+    //     // autoplay: 3000,
+    //     // gap: 30
+    // }).mount()
+ 
 
 });
