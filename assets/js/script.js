0eac8ba783a942960a301c9cf147e1dd7903b1aa
gabilg <gabi_lima_xd@hotmail.com>
2019-10-02 00:39:09 -0300

    back button nav fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,21 +37,28 @@ $(document).ready(function() {
     });
 
 
-    $(window).scroll(function() {
-        
-        const is_scroll = $(this).scrollTop() > 80;
+    function changeNavColor(_this) {
+
+        const is_scroll = $(_this || window).scrollTop() > 80;
 
         var pageURL = window.location.pathname;
-
+    
         if (pageURL.indexOf('index') >= 0 || pageURL === '/') {
-
+    
             $('.nav').toggleClass('scroll', is_scroll);
             $('.container-logo').toggleClass('scroll', is_scroll);
             $('.logo-img').toggleClass('scroll', is_scroll);
             $('.dropdown-menu').toggleClass('scroll', is_scroll);
-
+    
         }
+    }
+
+    changeNavColor()
+    
+
+    $(window).scroll(function() {
         
+     changeNavColor(this)
 
         // if ($('.nav').hasClass('scroll')) return
 
