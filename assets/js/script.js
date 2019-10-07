53b93ba857007ba352b388ff5968cd72a54ea535
gabilg <gabi_lima_xd@hotmail.com>
2019-10-07 02:12:12 -0300

    line-height fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ $(document).ready(function() {
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-        scrollTop: $(hash).offset().top - 100
+        scrollTop: $(hash).offset().top - 90
         }, 800, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
